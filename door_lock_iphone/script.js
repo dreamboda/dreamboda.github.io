ee759d4d2536f67f5921193bb97f6bea8ad4d1b3
dreambig <114136246+dreamboda@users.noreply.github.com>
2023-06-10 09:43:58 +0800

    Update script.js

--- door_lock_iphone/script.js
+++ door_lock_iphone/script.js
@@ -1,6 +1,6 @@
 // 定義名稱。要與 images 資料夾內相同名稱 
-var labels = prompt("請輸入名稱並以逗號隔開人名:","Teddy,Chuan").toString().split(",")
-var username = prompt("請輸入 AIO 使用者名稱:","flagweng")
+var labels = prompt("請輸入名稱並以逗號隔開人名:","boda").toString().split(",")
+var username = prompt("請輸入 AIO 使用者名稱:","yang_buda")
 
 const video1 = document.getElementById('inputVideo')
 const conDev = document.getElementById('connDiv') 
